d324cc4e67db16ba2b126fe4b44586715fbbcb48
Kaspar Martin Suursalu <kasparsu@gmail.com>
2022-05-04 17:44:20 +0300

    add shorter option

--- js.js
+++ js.js
@@ -1,15 +1,1 @@
-/* const $ = document.querySelector.bind(document);
-let closeOpFunc = () => {
-    $('.nav-burger').classList.toggle('toggle');
-}
-$('.btn-nav').addEventListener("click", closeOpFunc); 
-$('.nav-burger').addEventListener("click", closeOpFunc); */
-
-
-document.querySelectorAll('.btn-nav, .nav-burger').forEach(e => {
-    e.addEventListener("click", () => {
-        document.querySelector('.nav-burger').classList.toggle('toggle');
-    });
-});
-
-
+let toggle = () => document.querySelector('.nav-burger').classList.toggle('toggle');
